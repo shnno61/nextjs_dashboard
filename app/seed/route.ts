2e737207871e9dbb56edbaf02c8b52f40a11f616
ahmed elshnnawy <ahmedelshnawy91@gmail.com>
2024-07-13 15:38:36 +0300

    seed success

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,5 +1,5 @@
 import bcrypt from 'bcrypt';
-import { db } from '/@vercel/postgres';
+import { db } from '@vercel/postgres';
 import { invoices, customers, revenue, users } from '../lib/placeholder-data';
 
 const client = await db.connect();
@@ -102,10 +102,7 @@ async function seedRevenue() {
 }
 
 export async function GET() {
-  return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  });
+  
   try {
     await client.sql`BEGIN`;
     await seedUsers();
